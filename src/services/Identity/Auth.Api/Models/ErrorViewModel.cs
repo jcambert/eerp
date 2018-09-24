36a685e64018fdb9f89589c97b7b3ff156e0e818
Ambert Jean-Christophe <jc.ambert@free.fr>
2018-09-24 17:53:39 +0200

    Ping Auth

---
+++ src/services/Identity/Auth.Api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Auth.Api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- src/web/ping/ePing/wwwroot/js/site.js
+++ src/web/ping/ePing/wwwroot/js/site.js
@@ -2,9 +2,40 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var app = new Vue({
+new Vue({
     el: '#app',
-    data: {
-        message: 'Bonjour Vue!'
+    data: () => ({
+        drawer: null,
+        message:'BONJOUR !!!',
+        items: [
+            { icon: 'lightbulb_outline', text: 'Notes' },
+            { icon: 'touch_app', text: 'Reminders' },
+            { divider: true },
+            { heading: 'Labels' },
+            { icon: 'add', text: 'Create new label' },
+            { divider: true },
+            { icon: 'archive', text: 'Archive' },
+            { icon: 'delete', text: 'Trash' },
+            { divider: true },
+            { icon: 'settings', text: 'Settings' },
+            { icon: 'chat_bubble', text: 'Trash' },
+            { icon: 'help', text: 'Help' },
+            { icon: 'phonelink', text: 'App downloads' },
+            { icon: 'keyboard', text: 'Keyboard shortcuts' }
+        ]
+    }),
+    props: {
+        source: String
+    }
+})
+
+new Vue({
+    el: '#app',
+    data: () => ({
+        drawer: null
+    }),
+
+    props: {
+        source: String
     }
 })
