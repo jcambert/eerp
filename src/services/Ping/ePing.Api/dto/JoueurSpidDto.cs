131356a9a397b21827b78136fc10f16a8c9eea69
Ambert Jean-Christophe <jc.ambert@free.fr>
2018-10-10 17:51:15 +0200

    Ping

---
+++ src/services/Ping/ePing.Api/dto/JoueurSpidDto.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace ePing.Api.dto
+{
+
+    [DataContract]
+    public class ListeJoueurHeader
+    {
+        [DataMember(Name = "liste")]
+        [JsonProperty("liste")]
+        public JoueurDtoHeader Liste { get; set; }
+    }
+
+    public class JoueurDtoHeader
+    {
+        [DataMember(Name = "joueur")]
+        [JsonProperty("joueur")]
+        public JoueurSpidDto Joueur { get; set; }
+
+    }
+
+    [DataContract]
+    public class ListeJoueursHeader
+    {
+        [DataMember(Name ="liste")]
+        [JsonProperty("liste")]
+        public JoueursDtoHeader Liste { get; set; }
+    }
+
+    public class JoueursDtoHeader
+    {
+        [DataMember(Name ="joueur")]
+        [JsonProperty("joueur")]
+        public List<JoueurSpidDto> Joueurs { get; set; }
+
+    }
+    public class JoueurSpidDto
+    {
+        [DataMember(Name = "licence")]
+        [JsonProperty("licence")]
+        public string Licence { get; set; }
+        [DataMember(Name = "nom")]
+        [JsonProperty("nom")]
+        public string Nom { get; set; }
+        [DataMember(Name = "prenom")]
+        [JsonProperty("prenom")]
+        public string Prenom { get; set; }
+        [DataMember(Name = "club")]
+        public string Club { get; set; }
+        [DataMember(Name = "sexe")]
+        [JsonProperty("sexe")]
+        public string Sexe { get; set; }
+        [DataMember(Name = "echelon")]
+        [JsonProperty("echelon")]
+        public string Echelon { get; set; }
+        [DataMember(Name = "place")]
+        [JsonProperty("place")]
+        public string Place { get; set; }
+        [DataMember(Name = "point")]
+        [JsonProperty("point")]
+        public double Point { get; set; }
+
+        [DataMember(Name = "natio")]
+        [JsonProperty("natio")]
+        public string Nationalite { get; set; }
+        [DataMember(Name = "clglob")]
+        [JsonProperty("clglob")]
+        public int ClassementGlobal { get; set; }
+        [DataMember(Name = "aclglob")]
+        [JsonProperty("aclglob")]
+        public int AncienClassementGlobal { get; set; }
+        [DataMember(Name = "apoint")]
+        [JsonProperty("apoint")]
+        public double AncienPoint { get; set; }
+        [DataMember(Name = "clast")]
+        [JsonProperty("clast")]
+        public int Classement { get; set; }
+        [DataMember(Name = "clnat")]
+        [JsonProperty("clnat")]
+        public int ClassementNational { get; set; }
+        [DataMember(Name = "categ")]
+        [JsonProperty("categ")]
+        public string Categorie { get; set; }
+        [DataMember(Name = "rangreg")]
+        [JsonProperty("rangreg")]
+        public int RangRegional { get; set; }
+        [DataMember(Name = "rangdep")]
+        [JsonProperty("rangdep")]
+        public int RangDepartemental { get; set; }
+        [DataMember(Name = "valcla")]
+        [JsonProperty("valcla")]
+        public int PointOfficiel { get; set; }
+        [DataMember(Name = "clpro")]
+        [JsonProperty("clpro")]
+        public int PropositionClassement { get; set; }
+        [DataMember(Name = "valinit")]
+        [JsonProperty("valinit")]
+        public double PointDebut { get; set; }
+
+
+    }
+}
---
+++ src/services/Ping/ePing.Api/models/JoueurSpid.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ePing.Api.models
+{
+    public class JoueurSpid:Trackable
+    {
+        private string _categorie;
+
+        [Key]
+        public string Licence { get; set; }
+
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        [IgnoreMap]
+        public Club Club { get; set; }
+
+        public string Sexe { get; set; }
+
+        public string  Echelon { get; set; }
+
+        public string Place { get; set; }
+
+        public double Point { get; set; }
+
+        //public int Classe =>(Point/100);
+
+
+        public string Nationalite { get; set; }
+
+        public int ClassementGlobal { get; set; }
+
+        public int AncienClassementGlobal { get; set; }
+
+        public double AncienPoint { get; set; }
+
+        public int Classement { get; set; }
+
+        public int ClassementNational { get; set; }
+
+        public string Categorie { get { return _categorie ?? "N"; } set { _categorie = value; } }
+
+        public int RangRegional { get; set; }
+
+        public int RangDepartemental { get; set; }
+
+        public int PointOfficiel { get; set; }
+
+        public int PropositionClassement { get; set; }
+
+        public double PointDebut { get; set; }
+
+        public double ProgressionMensuelle =>  Point- (AncienPoint==0?500:AncienPoint);
+
+        public double ProgressionAnnuelle => Point-(PointDebut==0?500: PointDebut);
+        [IgnoreMap]
+        public Joueur Extra { get; set; }
+
+    }
+}
